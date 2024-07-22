6e2df84c75fe500325f4e4f85277bbcb3bf9be42
Artem <106353839+paranoiaSTFU@users.noreply.github.com>
2024-07-22 03:11:47 +0300

    test
    ...

---
+++ 1. Lesson One/PHP/Index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Изучение PHP</title>
+</head>
+<body>
+    <?php 
+        include '1.php'
+    ?>
+</body>
+</html>
